7e02a4824d8596f615d84b5d1c6bc4a7729792f6
Tomas <210099@fppiramide.com>
2022-01-20 00:53:40 +0100

    Agregue clase coleccion

--- src/ColeccionLibros.java
+++ src/ColeccionLibros.java
@@ -1,4 +1,59 @@
 
+import java.util.ArrayList;
+
 public class ColeccionLibros {
+
+	private ArrayList<Libro> coleccion;
+
+	public ColeccionLibros() {
+		coleccion = new ArrayList<>();
+	}
+
+	public int sizeColeccion() {
+		return coleccion.size();
+	}
+
+	public void insertarLibro(Libro libro) {
+
+		if (coleccion.size() > 0) {
+
+			for (int i = 0; i < coleccion.size(); i++) {
+
+				if (coleccion.get(i).getTitulo().compareTo(coleccion.get(i+1).getTitulo()) == -1
+						&& coleccion.get(i).getTitulo().compareTo(coleccion.get(i-1).getTitulo()) == 1) {
+					for (int j = coleccion.size()-1; j > i; j--) {
+						
+						coleccion.add(j+1, coleccion.get(j));
+					}
+					coleccion.add(i,libro);
+				}
+
+			}
+
+		}
+		coleccion.add(libro);
+	}
+
+	public void eliminarLibro(int index) {
+
+		coleccion.remove(index);
+	}
+
+	public Libro getLibro(int index) {
+		return coleccion.get(index);
+	}
+
+	@Override
+
+	public String toString() {
+		String listaLibros = "Coleccion: \n";
+
+		for (Libro libro : coleccion) {
+			listaLibros += libro.toString();
+			
+			listaLibros += "\n\n";
+		}
+		return listaLibros;
+	}
 
 }
--- src/PruebaLibro.java
+++ src/PruebaLibro.java
@@ -11,6 +11,13 @@ public class PruebaLibro {
 		autor1.setApellidos("inaros");
 		autor1.setPaisNacimiento("Myammar");
 		
+		Autor autor2= new Autor();
+		
+		autor2.setNombre("Antoine");
+		autor2.setApellidos("de Saint");
+		autor2.setPaisNacimiento("Francia");
+		
+		ColeccionLibros coleccion1 = new ColeccionLibros();
 		
 		
 		Libro libro1= new Libro();
@@ -22,7 +29,39 @@ public class PruebaLibro {
 		libro1.setLugarPubli("higiro", "Monge", "Burundi");
 		libro1.setfechaPublicacion("2", "4", "1998");
 		libro1.setPaginas(981);
-		System.out.println(libro1);
+		
+		coleccion1.insertarLibro(libro1);
+		
+		
+		Libro libro2= new Libro();
+		libro2.setAutor(autor1);
+		libro2.setTitulo("El principito");
+		libro2.setEdicion("1ª edicion");
+		libro2.setCodigo("1231823-X");
+		libro2.setLugarPubli("Kubre", "Toiloisse", "Francia");
+		libro2.setfechaPublicacion("2", "4", "1943");
+		libro2.setPaginas(981);
+		
+		coleccion1.insertarLibro(libro2);
+		
+		
+		Libro libro3= new Libro();
+		libro3.setAutor(autor1);
+		libro3.setTitulo("Zarai");
+		libro3.setEdicion("1ª edicion");
+		libro3.setCodigo("12318243-X");
+		libro3.setLugarPubli("Kubre", "Toiloisse", "España");
+		libro3.setfechaPublicacion("2", "4", "1943");
+		libro3.setPaginas(981);
+		
+		coleccion1.insertarLibro(libro3);
+		
+		
+		
+		
+		System.out.println(coleccion1);
+		
+		
 
 	}
 
